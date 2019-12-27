b77f80dde2d1638e50095e082f29d44d55938efa
juanfstillo <53223694+juanfstillo@users.noreply.github.com>
2019-12-27 19:15:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//var eltitulo = document.querySelector('p');
+//console.log(eltitulo);
+
+var eltitulo = document.querySelectorAll('p');
+console.log(eltitulo);
+
+var enlace = document.querySelector('.enlace');
+console.log(enlace);
+
+var porid = document.getElementById('tituloprincipal');
+console.log(porid);
---
+++ index2.js
@@ -0,0 +1,32 @@
+var auto = {
+  marca:'fiat',
+  kilometraje:2290,
+  color:'negro',
+  dueño:'Juan',
+  arrancar: function (){
+    return 'runrun';
+  }
+}
+
+//console.log('tengo un auto de marca ' +auto.marca+ ' que tiene ' +auto.kilometraje+ ' kilometros de color '+auto.color+' y que hace '+ auto.arrancar());
+/*var alumno = {
+  nombre: 'Martin',
+  edad: 37,
+  altura: 1.56,
+  email: 'mm@dh.com.ar',
+}
+for (var propiedades in alumno) {
+  console.log(alumno[propiedades]);
+}
+*/
+//alert('Cucurucho');
+
+//var user = prompt('say my name');
+//console.log(user);
+
+//var consulta = confirm('Estas de acuerdo');
+//if (consulta==true) {
+//  console.log ('Hola susana');
+//}else {
+//  console.log('chau susana');
+//}
